d934fd40135329b77389aa97a752caef596080ef
binqnb18 <145347915+binqnb18@users.noreply.github.com>
2024-07-10 18:26:40 +0700

    cập nhật bản database

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,17 +4,19 @@ import Footer from "./components/FE_CLIENT/Layouts/Footer";
 import Header from "./components/FE_CLIENT/Layouts/Navbar";
 import Banner from './components/FE_CLIENT/Page/Banner';
 import Home from "./components/FE_CLIENT/Page/Home";
+import About from "./components/FE_CLIENT/Page/About/About";
 const App = () => {
     return (
         <>
-    <Header />
+    {/* <Header />
         <BrowserRouter>
             <Routes>
                     <Route path="/" element={<Home />}>
                 </Route>
             </Routes>
         </BrowserRouter>
-    <Footer/>
+    <Footer/> */}
+    <About />
         </>
        
     );
