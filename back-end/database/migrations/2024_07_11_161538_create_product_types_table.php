5ef46b630661e0bc770ac3aabbc3d35924a9017a
QuocThang168 <169134842+QuocThang168@users.noreply.github.com>
2024-07-12 00:10:33 +0700

    new migration

--- back-end/database/migrations/2024_07_11_161538_create_product_types_table.php
+++ back-end/database/migrations/2024_07_11_161538_create_product_types_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('adderss');
-            $table->string('phone_number');
-            $table->tinyInteger('role_id')->default(0);
-            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_types');
     }
 };
--- back-end/database/migrations/2024_07_11_162559_create_news_table.php
+++ back-end/database/migrations/2024_07_11_162559_create_news_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('adderss');
-            $table->string('phone_number');
-            $table->tinyInteger('role_id')->default(0);
+            $table->string('title');
+            $table->string('description');
+            $table->string('content_link');
+            $table->string('thumbnail');
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news');
     }
 };
