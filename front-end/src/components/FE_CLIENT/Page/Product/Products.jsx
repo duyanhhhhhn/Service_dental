53c828329fd8144524ac78bafb650e219ce7838d
Duyanh <nguyenduyanh260305a@gmail.com>
2024-07-18 14:49:03 +0700

    Product

--- front-end/src/components/FE_CLIENT/Page/Product/Products.jsx
+++ front-end/src/components/FE_CLIENT/Page/Product/Products.jsx
@@ -1,18 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Image, Container, Row, Col, Card, Button } from "react-bootstrap";
 
 const Products = () => {
-    const [product, setProduct] = useState([]);
+    const [products, setProducts] = useState([]);
+    
     useEffect(() => {
-         axios.get('products').then(resp =>
-        setProduct(resp.data))
-    }, [])
-    console.log(product)
-    return (<div>
-        {product?.map((item, index) => (
-            <div key={index}>{item.name}</div>
-        ))}
-    </div>  );
+        axios.get('products').then(resp => setProducts(resp.data));
+    }, []);
+    
+    console.log(products);
+    
+    return (
+        <Container>
+            <Row>
+                {products?.map((item, index) => (
+                    <Col key={index} md={4} className="mb-4 my-5">
+                        <Card className="h-100" style={{border:'1px solid #333'}}>
+                            <Image src={item.thumbnail} alt="product" fluid className="card-img-top" style={{border:'1px solid #ccc'}}/>
+                            <Card.Body>
+                                <Card.Title>{item.name}</Card.Title>
+                                <Card.Text>
+                                    <span className="text-primary">{item.price}$</span>
+                                    <br />
+                                    <span className="text-gray-500">{item.description}</span>
+                                </Card.Text>
+                                <Button  className="btn btn-primary btn-block">
+                                  Add to cart
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
- 
+
 export default Products;
